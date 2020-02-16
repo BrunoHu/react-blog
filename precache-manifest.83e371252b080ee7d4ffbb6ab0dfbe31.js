36666ff065fcafdce64708a33600e554f9106b57
bates-hu <hbc0204@foxmail.com>
2020-02-15 20:53:04 -0800

    Updates

---
+++ precache-manifest.83e371252b080ee7d4ffbb6ab0dfbe31.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8c3d0d017eb320a7276c8092df45c7c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "83550e8a7b03e792a1aa",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "a4968754e617706302d2",
+    "url": "/static/js/2.e39e7b95.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.e39e7b95.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83550e8a7b03e792a1aa",
+    "url": "/static/js/main.122118df.chunk.js"
+  },
+  {
+    "revision": "91de3d6c4e87c90b7d90",
+    "url": "/static/js/runtime-main.7ffca68f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
